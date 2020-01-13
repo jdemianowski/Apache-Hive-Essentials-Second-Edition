711176cdc5b54ae04fce374cba39a85e2f3f4734
Jakub Demianowski <jakub.demianowski2@orange.com>
2020-01-13 22:05:46 +0100

    nowe skrypty

---
+++ my_scripts/config_info.sh
@@ -0,0 +1,109 @@
+sudo /opt/cloudera/installer/uninstall-cloudera-manager.sh
+sudo rm -rf /opt/cloudera/
+sudo rm -rf /etc/yum.repos.d/cloudera-manager.repo*
+
+sudo wget https://archive.cloudera.com/cm7/7.0.3/redhat7/yum/cloudera-manager-trial.repo -P /etc/yum.repos.d/
+sudo rpm --import https://archive.cloudera.com/cm7/7.0.3/redhat7/yum/RPM-GPG-KEY-cloudera
+sudo yum makecache
+sudo yum update
+
+wget https://archive.cloudera.com/cm7/7.0.3/cloudera-manager-installer.bin
+wget https://archive.cloudera.com/cm6/6.3.2/cloudera-manager-installer.bin
+
+
+chmod u+x cloudera-manager-installer.bin
+sudo ./cloudera-manager-installer.bin
+
+netstat -tulnp | grep 7180
+
+sudo groupadd supergroup
+sudo usermod -a -G supergroup mapred
+sudo usermod -a -G supergroup hdfs
+sudo hdfs dfs -chmod 775 /
+sudo usermod -a -G supergroup hive
+
+set yarn.nodemanager.resource.memory-mb=8096
+
+yarn.scheduler.minimum-allocation-mb: 2048
+yarn.scheduler.maximum-allocation-mb: 8096
+
+yarn.scheduler.minimum-allocation-vcores=1
+yarn.scheduler.maximum-allocation-vcores=4
+
+
+how to store user & pass for beeline:
+
+# https://community.cloudera.com/t5/Support-Questions/Sqoop-stuck-What-should-I-do/td-p/229726
+
+sudo vim /etc/hive/conf/beeline-hs2-connection.xml
+
+<?xml version="1.0"?>
+<configuration>
+<property>
+  <name>beeline.hs2.connection.user</name>
+  <value>osboxes</value>
+</property>
+<property>
+  <name>beeline.hs2.connection.password</name>
+  <value>cloudera</value>
+</property>
+</configuration>
+
+yarn.scheduler.capacity.maximum-am-resource-percent = 0.8
+
+hdfs dfs -rm -f -r  hdfs://osboxes.local:8020/user/osboxes/*
+
+yum install stress-ng -y
+stress-ng --cpu 4 --perf -t 60
+
+# install sample databases
+sudo postgresql-setup initdb
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
+
+psql -d template1
+createdb
+exit
+
+sudo -u postgres -i
+psql
+alter user kuba createdb;
+ALTER USER kuba WITH SUPERUSER;
+alter user kuba with password 'cloudera';
+exit
+
+git clone https://github.com/morenoh149/postgresDBSamples.git
+cd adventureworks
+unzip data.zip
+psql -c "CREATE DATABASE \"adventureworks\";"
+psql -d Adventureworks < install.sql
+cd ..
+cd worldDB-1.0/
+psql -c "CREATE DATABASE worlddb;" < world.sql
+cd ..
+cd usda-r18-1.0/
+createdb usda
+psql -f usda.sql usda
+cd ..
+cd iso-3166/
+createdb iso-3166
+psql -f iso-3166.sql iso-3166
+cd ..
+cd chinook-1.4/
+createdb -E UTF8 chinook
+psql -f Chinook_PostgreSql_utf8.sql -d chinook
+
+sudo vim /var/lib/pgsql/data/postgresql.conf
+# add at the end of the file:
+listen_addresses = '*'
+
+sudo vim /var/lib/pgsql/data/pg_hba.conf
+# add at the end of the file:
+host    all             all              0.0.0.0/0                       md5
+host    all             all              ::/0                            md5
+
+sudo  systemctl restart postgresql
+
+# check
+netstat -nlt | grep 5432
+
---
+++ my_scripts/script_6.hql
@@ -0,0 +1,45 @@
+use worlddb;
+
+SET hive.exec.dynamic.partition=true;
+SET hive.exec.dynamic.partition.mode=nonstrict;
+SET hive.optimize.sort.dynamic.partition=true;
+SET hive.exec.max.dynamic.partitions=500000;
+SET hive.exec.max.dynamic.partitions.pernode=500000;
+set hive.enforce.bucketing=true;
+
+--
+select * from city
+distribute by rand() sort by rand() limit 10;
+
+drop table if exists city_orc;
+CREATE TABLE if not exists `city_orc`(
+  `id` int,
+  `name` string,
+  `district` string,
+  `population` int)
+PARTITIONED BY (countrycode string)
+stored as orc;
+insert OVERWRITE TABLE city_orc partition (countrycode)
+select id, name, district, population, countrycode from city;
+
+
+select count(1) from city_orc where countrycode = 'ARG' or countrycode = 'BRA';
+
+select count(1) from city where countrycode = 'ARG' or countrycode = 'BRA';
+
+SET hive.exec.compress.intermediate=true;
+SET hive.intermediate.compression.codec=org.apache.hadoop.io.compress.SnappyCodec;
+
+select * from city_orc limit 10;
+-- 24 s.
+select sum(population) from city group by (countrycode);
+-- 38/40 s.
+select cr.name, sum(co.population), avg(co.population), count(*)
+from city_orc as co join country cr on cr.code = co.countrycode
+group by (cr.name);
+
+-- 4.8 s.
+select * from city order by population;
+
+-- 2 s.
+select * from (select * from city CLUSTER BY population) t order by population;
